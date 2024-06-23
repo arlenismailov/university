a77c7ddb891c87d448a4d8f38fbdc53253bcadbc
Arlen <141837914+arlenismailov@users.noreply.github.com>
2024-06-23 20:42:36 +0600

    Delete univer_pro directory

--- univer_pro/univer_app/translation.py
+++
@@ -1,7 +0,0 @@
-from .models import *
-from modeltranslation.translator import TranslationOptions,register
-
-
-@register(AboutUniversity)
-class ProductTranslationOptions(TranslationOptions):
-    fields = ('name', 'description')
